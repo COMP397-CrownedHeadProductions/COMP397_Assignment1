36bccb60b8cb82209b373c33437f4be02f5c4252
Peitong Liu <vincentliu9816@gmail.com>
2021-02-13 23:08:59 -0500

    Settings Menu added
    Volume slider not functional yet.

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    /// <summary>
+    /// THIS SCRIPT WILL BE HANDLING ALL FUNCTIONS USED BY THE MENU, END GAME SCENE, 
+    /// OPTION AND PAUSE MENU.
+    /// </summary>
+    
+
+    //Menu and end game scene functions.
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -16,5 +24,16 @@ public class MenuController : MonoBehaviour
     public void RestartGame()
     {
         SceneManager.LoadScene(0);
+    }
+    public void ToMenu()
+    {
+        SceneManager.LoadScene("TestScene_SettingsScene");
+    }
+    //Option function(s).
+    public AudioMixer audioMixer;
+    public void SetVolume(float vol)
+    {
+        Debug.Log(vol);
+        audioMixer.SetFloat("Volume", vol);
     }
 }
