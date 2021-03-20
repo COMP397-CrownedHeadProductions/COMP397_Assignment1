9964ae6745f15761efec54e5258079c61fd88bbf
Timothy Garcia <timothyiangarcia@gmail.com>
2021-03-20 01:49:40 -0400

    floating platform animation updated (WIP)

--- Assets/Scripts/PlatformAttach.cs
+++ Assets/Scripts/PlatformAttach.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class PlatformAttach : MonoBehaviour
 {
     public GameObject player;
+    public GameObject pivot;
+    public GameObject mainCamera;
 
-
+    void Start()
+    {
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject == player)
         {
             player.transform.parent = transform;
+            pivot.transform.parent = transform;
+            mainCamera.transform.parent = transform;
         }       
     }
     private void OnTriggerExit(Collider other)
@@ -19,9 +25,8 @@ public class PlatformAttach : MonoBehaviour
         if (other.gameObject == player)
         {
             player.transform.parent = null;
+            pivot.transform.parent = null;
+            mainCamera.transform.parent = null;
         }        
     }
-
-
-
 }
